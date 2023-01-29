c1e06fb72e7932095ce6861efe074ba42c4f37d5
Arun Krishnamurthy <arun.km.279@gmail.com>
2023-01-29 16:41:46 -0600

    working on arrays

---
+++ array/1-two-sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        pairs = {}
+        for i, num in enumerate(nums):
+            pair = target - num
+            if pair in pairs:
+                return [i, pairs[pair]]
+            pairs[num] = i
+
+s = Solution()
+
+# Example 1
+nums = [2, 7, 11, 15]
+target = 9
+assert s.twoSum(nums, target) == [0, 1]
+
+# Example 2
+nums = [3, 2, 4]
+target = 6
+assert s.twoSum(nums, target) == [1, 2]
+
+# Example 3
+nums = [3, 3]
+target = 6
+assert s.twoSum(nums, target) == [0, 1]
